86e52f80ad4ac35b606d861733bc2fd2c838a0a6
cilafreitas <cila123xavier@gmail.com>
2023-07-31 12:51:22 -0300

    configurando firebase

--- src/main.js
+++ src/main.js
@@ -1,5 +1,21 @@
 // Este es el punto de entrada de tu aplicacion
 
-import { myFunction } from './lib/index.js';
+import { myFunction } from "./lib/index.js";
+// Import the functions you need from the SDKs you need
+
+const email = "teste@gmail.com";
+const password = "123456";
+
+createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ..
+  });
 
 myFunction();
